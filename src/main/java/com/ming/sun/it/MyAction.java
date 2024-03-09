d224d3da2ba3a2270d4ade0252a8ba6d67a433fb
yangxiaoming <18340850070@163.com>
2024-03-09 10:33:11 +0800

    插件开发正式模板,jdk17

--- src/main/java/com/ming/sun/it/MyAction.java
+++ src/main/java/com/ming/sun/it/MyAction.java
@@ -1,12 +1,16 @@
-package com.mingsun.it;
+package com.ming.sun.it;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.diagnostic.Logger;
 
 public class MyAction extends AnAction {
+    private static final Logger LOG = Logger.getInstance(MyAction.class);
+
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
+        LOG.info("hello log.....");
         System.out.println("hello");
     }
 }
---
+++ src/main/java/com/ming/sun/it/MyToolWindowFactory.java
@@ -0,0 +1,31 @@
+package com.ming.sun.it;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import org.jetbrains.annotations.NotNull;
+
+public class MyToolWindowFactory implements ToolWindowFactory {
+    @Override
+    public boolean isApplicable(@NotNull Project project) {
+        return ToolWindowFactory.super.isApplicable(project);
+    }
+
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        System.out.println("hello window");
+
+    }
+
+    @Override
+    public void init(@NotNull ToolWindow toolWindow) {
+        ToolWindowFactory.super.init(toolWindow);
+    }
+
+    @Override
+    public boolean shouldBeAvailable(@NotNull Project project) {
+        return ToolWindowFactory.super.shouldBeAvailable(project);
+    }
+
+
+}
